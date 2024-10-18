ef66e699b05babb56070b84c7efd1d90adbfd1ae
pigozzif <pigozzife@gmail.com>
2024-10-17 23:42:18 -0400

    parallel main

---
+++ train.py
@@ -0,0 +1,111 @@
+import os
+
+import gymnasium.spaces
+import numpy as np
+from matplotlib import pyplot as plt
+from sb3_contrib import RecurrentPPO
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
+
+from algorithms import ExhaustiveSolver
+from envs import MotionEquation, GRNEnv, LotkaVolterraEquation, SchrodingerEquation
+from grn import GeneRegulatoryNetwork
+from plotting import plot_reward
+from utils import parse_args, set_seed, get_file_name, create_system_rollout_module, discrete2continuous
+
+
+def get_env(env_name, seed):
+    if env_name == "motion":
+        return MotionEquation()
+    elif env_name == "lotkavolterra":
+        return LotkaVolterraEquation()
+    elif env_name == "schrodinger":
+        return SchrodingerEquation()
+    elif "-" in env_name and all(map(lambda x: x.isnumeric(), env_name.split("-"))):
+        ids = env_name.split("-")
+        grn = GeneRegulatoryNetwork.create(biomodel_idx=int(ids[0]))
+        return GRNEnv(seed=seed,
+                      grn=grn,
+                      obs_dim=len(create_system_rollout_module(grn.config).grn_step.y_indexes),
+                      r=ids[1],
+                      ucs=ids[2],
+                      cs=ids[3])
+    raise ValueError("Invalid env name: {}".format(env_name))
+
+
+def get_algorithm(algorithm, **kwargs):
+    if algorithm == "ppo":
+        return PPO(**kwargs)
+    elif algorithm == "rppo":
+        return RecurrentPPO(**kwargs)
+    elif algorithm == "exhaustive-single":
+        return ExhaustiveSolver(env=kwargs["env"], seed=kwargs["seed"])
+    raise ValueError("Invalid algorithm name: {}".format(algorithm))
+
+
+def train(seed, task, algorithm, policy, num_steps=int(5e3)):
+    file_name = get_file_name(seed=seed, task=task, algorithm=algorithm, policy=policy)
+    env = Monitor(env=get_env(env_name=task, seed=seed),
+                  filename=os.path.join("output", "monitor.csv"),
+                  info_keywords=("is_broken",))
+    model = get_algorithm(algorithm=algorithm, seed=seed, policy=policy, env=env, verbose=1)
+    # model.load(os.path.join("models", file_name))
+    # return model
+    model.learn(total_timesteps=num_steps, progress_bar=True)
+    model.save(os.path.join("models", file_name))
+    os.rename(os.path.join("output", "monitor.csv"), os.path.join("output", file_name + ".csv"))
+    return model
+
+
+def evaluate(model):
+    mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=1)
+    return mean_reward, std_reward
+
+
+def save_rendering(model, file_name, num_steps=1):
+    vec_env = model.get_env()
+    obs = vec_env.reset()
+    data = []
+    actions = []
+    lstm_states = None
+    episode_starts = np.ones((1,), dtype=bool)
+    env = vec_env.envs[0].env
+    fig, axes = plt.subplots(figsize=(8, 10), nrows=2, ncols=1)
+    for i in range(num_steps):
+        action, lstm_states = model.predict(obs, state=lstm_states, episode_start=episode_starts, deterministic=True)
+        obs, rewards, dones, info = vec_env.step(action)
+        episode_starts = dones
+        data.append(info[0]["full_obs"])
+        if isinstance(env.action_space, gymnasium.spaces.Discrete):
+            action = discrete2continuous(action=action,
+                                         env=env,
+                                         num_steps=info[0]["full_obs"].shape[1])
+        actions.append(action)
+    axes[0].plot(np.hstack(data).T, label=env.get_species_names())
+    axes[0].set_title("species", fontsize=15)
+    axes[1].plot(np.hstack(actions), label=env.get_action_names())
+    axes[1].set_title("actions", fontsize=15)
+    for ax in axes:
+        ax.set_xlabel("sim. time [s]", fontsize=10)
+        ax.legend()
+    plt.savefig(os.path.join("render", file_name))
+    plt.close()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    set_seed(s=args.seed)
+    agent = train(seed=args.seed,
+                  task=args.task,
+                  algorithm=args.algorithm,
+                  policy=args.policy)
+    plot_reward(file_name=get_file_name(seed=args.seed,
+                                        task=args.task,
+                                        algorithm=args.algorithm,
+                                        policy=args.policy))
+    if args.render:
+        save_rendering(model=agent, file_name=".".join([get_file_name(seed=args.seed,
+                                                                      task=args.task,
+                                                                      algorithm=args.algorithm,
+                                                                      policy=args.policy), "png"]))
