b464cb32c2d8d6b97bc037bab2e42d3606bd3e13
pigozzif <pigozzife@gmail.com>
2024-10-23 16:18:44 -0400

    multiprocessing circuits

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from multiprocessing import Pool
 if __name__ == "__main__":
     # files = [file for file in os.listdir("memories") if file.endswith("pickle")]
     # for file in files:
-    print(sys.argv[1])
-    for biomodel_idx in pickle.load(open("circuits.pickle", "rb")):
+    # for biomodel_idx in pickle.load(open("circuits.pickle", "rb")):
         # os.system("python -W ignore al.py --task={}".format(biomodel_idx))
-        os.system("python -W ignore train.py -seed={0} --task={1} --algorithm=ga --render=False".format(sys.argv[1],
-                                                                                                        biomodel_idx))
-    # with Pool(4) as pool:
-    #     pool.map(os.system, ["python -W ignore train.py --task={}".format("-".join(file.split(".")[:-1]))
-    #                          for file in os.listdir("memories") if file.endswith("pickle")])
+    #     os.system("python -W ignore train.py -seed={0} --task={1} --algorithm=ga --render=False".format(sys.argv[1],
+    #                                                                                                     biomodel_idx))
+    with Pool(sys.argv[2]) as pool:
+        pool.map(os.system, ["python -W ignore train.py -seed={0} --task={1} --algorithm=ga --render=False"
+                 .format(sys.argv[1],
+                         biomodel_idx) for biomodel_idx in pickle.load(open("circuits.pickle", "rb"))])
