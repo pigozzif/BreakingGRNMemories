1dc4ae4dc2e4cc2eee972b4447220c74ec8799cd
pigozzif <pigozzife@gmail.com>
2024-10-23 16:27:43 -0400

    fix bugs

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         # os.system("python -W ignore al.py --task={}".format(biomodel_idx))
     #     os.system("python -W ignore train.py -seed={0} --task={1} --algorithm=ga --render=False".format(sys.argv[1],
     #                                                                                                     biomodel_idx))
-    with Pool(sys.argv[2]) as pool:
-        pool.map(os.system, ["python -W ignore train.py -seed={0} --task={1} --algorithm=ga --render=False"
+    with Pool(int(sys.argv[2])) as pool:
+        pool.map(os.system, ["python -W ignore train.py --seed={0} --task={1} --algorithm=ga --render=False"
                  .format(sys.argv[1],
                          biomodel_idx) for biomodel_idx in pickle.load(open("circuits.pickle", "rb"))])
